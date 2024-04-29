1bb79a875804aba700b1724334ef87f30b23c669
Enxos <23334313+Enxos@users.noreply.github.com>
2024-04-29 17:18:13 +0200

    TryNr2_Restart

---
+++ futoverseny/src/test/java/hu/fcomho/futoverseny/FutoversenyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fcomho.futoverseny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutoversenyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
